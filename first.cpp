d9c11975c720b73aca5811672dbcf322603c701b
Parkhomets <parkhomets_97@mail.ru>
2017-04-24 02:05:36 +0300

    Add files via upload
    Добавил функцию экстракта из внешнего файла! Корявенько, зато работает.
    Нужно в следующий раз: переделать запись имени и фамилии(чтоб вводились в одну строку)

--- first.cpp
+++ first.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
+#include <string>
 using namespace std;
 
 struct Node{
@@ -64,20 +66,53 @@ void List :: Show(int size){    //method of displaying list items
 }
 
 void List:: Ready_List(){ //here I will enter the reading list from external file
+    ifstream inFile1("B16505.txt",ios::in);
+    int iter = 0;
+    string s;
+    while (inFile1.peek() != EOF)
+    {
+        getline(inFile1, s);
+        iter++;
+    }
+    inFile1.close();
+    ifstream inFile("B16505.txt",ios::in);
+    const int len = 30;
+    const char ch = '\n';
+    char mass[len][iter];
+    if(!inFile) cout<<"incorrect input";
+    else{
+        for(int r=0; r<iter;r++)
+        {
+            inFile.getline(mass[r],len-1,ch);
+            size++;
+            Node *temp = new Node;
+            temp->Next = Head;
+            temp->Name = mass[r];
+            if (Head!=NULL){
+                Tail->Next = temp;
+                Tail = temp;
+            }
+            else{
+                Head = Tail = temp;
+            }
+        }
+    }
 
-
+    inFile.close();
 }
 
 int main(){
     List lst;
     int i;
-    cout<<"How many items will contain your list? Enter a number:";
+    /*cout<<"How many items will contain your list? Enter a number:";
     cin>>i;
     while (i!=0){
         lst.Add();
         i--;
     }
     cout<<"\n\n";
+    lst.Show(lst.size);*/
+    lst.Ready_List();
     lst.Show(lst.size);
     return 0;
 }
